c6448551db87fc8a1b8e22a477c3181c920db8a7
Ismail Ahmed <isahmed0149@gmail.com>
2022-06-19 10:44:08 -0400

    Update gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,13 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `UofTLUG Club Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.uoftlug.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: "/UofTLUG.github.io",
+  pathPrefix: "uoftlug.github.io",
   plugins: [],
 }
 
